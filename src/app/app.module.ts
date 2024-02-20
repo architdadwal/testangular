28af273743d15b8fa11a497efa55383bdd925109
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2024-02-20 14:59:34 +0530

    new
    updated more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppComponent } from './app.component';
-
 import { CircularDivsComponent } from './circular-divs/circular-divs.component';
+// import { CarouselComponent } from './carousel/carousel.component';
+// import { ParentComponent } from './parent/parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
-    CircularDivsComponent
+    CircularDivsComponent,
+    // CarouselComponent,
+    // ParentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/circular-divs/circular-divs.component.ts
+++ src/app/circular-divs/circular-divs.component.ts
@@ -7,22 +7,46 @@ import { Component } from '@angular/core';
 })
 export class CircularDivsComponent {
   showInfo: boolean = false;
+  infoHeading: string = '';
   infoDetails: string = '';
+  infoImage: string = ''; // New property for image URL
 
   showInfoDetails(info: string) {
     this.showInfo = !this.showInfo;
     switch(info) {
       case 'Product':
+        this.infoHeading = `Product`;
         this.infoDetails = `Learn about our products.`;
+        this.infoImage = 'assets/events.png'; // Set image URL for Product
         break;
       case 'Service':
+        this.infoHeading = `Services`;
         this.infoDetails = `Explore our services.`;
+        this.infoImage = 'assets/events.png'; // Set image URL for Service
         break;
       case 'Contact':
+        this.infoHeading = `Contact us `;
         this.infoDetails = `Contact us for assistance.`;
+        this.infoImage = 'assets/events.png'; // Set image URL for Contact
+        break;
+      case 'Category 4':
+        this.infoHeading = `Category 4`;
+        this.infoDetails = `Learn about Category 4.`;
+        this.infoImage = 'assets/events.png'; // Set image URL for Category 4
+        break;
+      case 'Category 5':
+        this.infoHeading = `Category 5`;
+        this.infoDetails = `Explore Category 5.`;
+        this.infoImage = 'assets/events.png'; // Set image URL for Category 5
+        break;
+      case 'Category 6':
+        this.infoHeading = `Category 6`;
+        this.infoDetails = `Contact us for assistance regarding Category 6.`;
+        this.infoImage = 'assets/events.png'; // Set image URL for Category 6
         break;
       default:
         this.infoDetails = '';
+        this.infoImage = ''; // Reset image URL if no valid case
     }
   }
 }
